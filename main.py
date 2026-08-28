5ec26a4c653a18763b2a1a027d83692f76b1ff2e
Marcos Iorio <marcossiorio@gmail.com>
2026-08-27 21:53:00 -0300

    Feature/load user texts (#1)
    * feat: add function to load texts scanned from the terminal
    
    * feat: update readme
    
    ---------
    
    Co-authored-by: Marcos Iorio <marcosiorio@MacBook-Pro-de-Marcos-2.local>

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
+def loadUserTexts():
+    first_text = str(input("Ingresá el primer texto: "))
+    second_text = str(input("Ingresá el segundo texto: "))
+
+    while first_text == "" or second_text == "":
+        if first_text == "":
+            first_text = str(input("Ingresá el primer texto: "))
+        else:
+            second_text = str(input("Ingresá el segundo texto: "))
+
+    return [first_text, second_text]
+
+
 def main():
-    pass
+    print("1) Cargar texto")
+
+    texts = loadUserTexts()
+
+    print(texts[0], texts[1])
+
+
+main()

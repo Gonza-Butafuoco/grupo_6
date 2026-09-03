f4e9f4b8d9440458ae0228622c2baf8af633fed3
Gonza-Butafuoco <butafuocogonzalo@gmai.com>
2026-09-03 12:53:16 -0300

    fix: corregir opciones del menú en main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import handler
 
 def main():
-    while True:
+    option = ""
+    while option != "0":
         print("\n--- TextCompare ---")
         print("1) Cargar textos para comparar")
-        print("2) Salir")
+        print("0) Salir")
         
         option = input("Elegí una opción: ")
 
         if option == "1":
             handler.start_comparison()
-        elif option == "2":
+        elif option == "0":
             print("Saliendo del sistema TextCompare...")
-            break
         else:
-            print("Opción inválida. Por favor, ingresá 1 o 2.")
+            print("Opción inválida. Por favor, ingresá 1 o 0.")
 
 main()

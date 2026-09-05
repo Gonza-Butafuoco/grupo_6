7e4895351280cc1e73f2e3e00ac820c58a3f04ce
tcitarella02 <tcitarella@uade.edu.ar>
2026-09-05 20:37:05 -0300

    Fix: Arreglo de calculate_similarity

--- actions.py
+++ actions.py
@@ -7,35 +7,33 @@ def calculate_similarity(list1, list2):
     # TODO:  Acá hay que sacar el porcentaje real (0 a 100).
     # Como el alcance pide usar conjuntos, pueden convertir las listas a set()
     # y sacar la intersección para ver cuánto se parecen.
-    new_list_1 = []
+    new_unique_list_1 = []
     for word in list1:
-        if word not in new_list_1:
-            new_list_1.append(word)
+        if word not in new_unique_list_1:
+            new_unique_list_1.append(word)
     
-    new_list_2 = []
+    new_unique_list_2 = []
     for word in list2:
-        if word not in new_list_2:
-            new_list_2.append(word)
-    
-    #Si ambas listas están vacias, son iguales
-    if len(new_list_1) == 0 and len(new_list_2) == 0:
-        return 100.0
+        if word not in new_unique_list_2:
+            new_unique_list_2.append(word)
     
     #Contar palabras en común
     words_in_common = 0
-    for word in new_list_1:
-        if word in new_list_2:
+    for word in new_unique_list_1:
+        if word in new_unique_list_2:
             words_in_common += 1
     
     #Contar palabras unicas entre ambas listas
-    total_words = len(new_list_1)
-    for word in new_list_2:
-        if word not in new_list_1:
-            total_words += 1
+    total_words = len(new_unique_list_1) + len(new_unique_list_2) - words_in_common
+    
+    #En caso que la división sea por cero, se retorna 0
+    if total_words == 0:
+        return 0.0
     
     #Calcular porcentaje de similitud
-    similarity_percentage = (words_in_common / total_words) * 100
-    return round(similarity_percentage, 2)  # Redondeo a 2 decimales
+    porcentaje_similitud = (words_in_common / total_words) * 100
+    
+    return round(porcentaje_similitud,2)
 
 def find_matches(list1, list2):
     """

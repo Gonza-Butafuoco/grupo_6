4e646cc14d0ca41bbb8997e656857d831468f7ad
irinapuskovas <ipuskovas@uade.utn.edu.ar>
2026-09-10 22:05:20 -0300

    Agregamos validacion de texto

--- utils.py
+++ utils.py
@@ -8,19 +8,27 @@ def process_text(text):
     Salida: Diccionario con lista de palabras finales y diccionario de la cantidad de veces que se repite las palabras
     """
 
+    # Validación del texto
+    if not text or not text.strip():
+        return None
+
     repeated_words = {}
     
-    words = text.lower().split()
+    #Pasamos a minuscula el textp
+    text = text.lower()
 
     # Expresion regular que cumpla con los caracteres
     patron = r'[^a-zA-ZáéíóúÁÉÍÓÚñÑ\s]'
-    textoNormalizado=re.sub(patron,'', words)
+    textoNormalizado=re.sub(patron,'', text)
+
+    #Separamos el texto
+    words = textoNormalizado.split()
     
     # filter y lambda descartan los elementos que quedaron vacíos
-    final_words = list(filter(lambda w: len(w) > 0, textoNormalizado))
+    final_words = list(filter(lambda w: len(w) > 0, words))
 
     for word in final_words:
-        repeated_words[word] = repeated_words.get[word, 0] + 1
+        repeated_words[word] = repeated_words.get(word, 0) + 1
 
     return {
         "normalized_text": final_words,
